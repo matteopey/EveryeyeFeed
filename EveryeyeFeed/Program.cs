77a30f4da86a7e2e091c20d22f6c5a9be39d216e
Matteo Peyronel <matteo.peyronel@deltatre.com>
2023-12-12 21:34:43 +0100

    Migrate to .NET 8 isolated

---
+++ EveryeyeFeed/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace EveryeyeFeed
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
